e1091b8bf370e503dfbba52bfc7c6942096dd365
shruthimg <shruthimg10@gmail.com>
2020-05-08 19:05:00 +0530

    Initial Commit

---
+++ spring-mvc-portlet/src/main/java/com/springmvc/portlet1/Portlet1ViewController.java
@@ -0,0 +1,36 @@
+/**
+1 * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.springmvc.portlet1;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+@Controller
+@RequestMapping("VIEW")
+public class Portlet1ViewController {
+
+	@RenderMapping
+	public String defaultPortlet1Render(RenderRequest renderRequest, RenderResponse renderResource , Model model) {
+		
+		renderRequest.setAttribute("name", "This is Portlet 1 JSP");		
+		return "view";
+	}
+
+}
